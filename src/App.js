435d7f761362216b9b4b76cd4e209fc797ed19d1
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-05 01:50:12 +0800

    CSS UwU :bikini:

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-    <div className="App">
-      <NavBar accounts={accounts} setAccounts={setAccounts} />
-      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    <div className="overlay">
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+      <div className="moving-background"></div>
     </div>
   );
 }
