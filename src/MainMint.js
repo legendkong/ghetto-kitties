2e76991f869a463e435b726ee7678bbab3637052
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-05 02:36:08 +0800

    Added chakra style comp for mainmint :pray:

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 // Package that allows you to connect to blockchain easily; has alot of utility functions, alternative for web3.js, industry standard now
 import { ethers, BigNumber } from 'ethers';
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
 import GhettoKittiesNFT from './GhettoKittiesNFT.json';
 
 const GhettoKittiesNFTAddress = "0x887F0e6af249F03Ab8228Dc06Feed7c4e60a059a";
@@ -39,22 +40,75 @@ const MainMint = ({ accounts, setAccounts}) => {
    };
 
    return (
-      <div>
-         <h1>Ghetto Kitties</h1>
-         <p>The kitties have been wandering the streets and forming their own hood. Will you be able to befriend one of them? Mint Ghetto Kitties to find out.</p>
+      <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+         <Box width="520px">
+         <div>
+         <Text fontSize="48px" textShadow="0 5px #000000">Ghetto Kitties</Text>
+            <Text 
+            fontSize="30px"
+            letterSpacing="-5.5%"
+            fontFamily="VT323"
+            textShadow="0 2px 2px #000000"
+            >The kitties have been wandering the streets and forming their own hood. Will you be able to befriend one of them? Mint Ghetto Kitties to find out.</Text>
+         </div>
+
+         
          {isConnected ? (
             <div>
-               <div>
-                  <button onClick={handleDecrement}>-</button>
-                  <input type="number" value={mintAmount} />
-                  <button onClick={handleIncrement}>+</button>
-               </div>
-               <button onClick={handleMint}>Mint Now</button>
+               <Flex align="center" justify="center">
+                  <Button 
+                  backgroundColor="#D6517D"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0F0F0F"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  margin="0 15px" 
+                  onClick={handleDecrement}>-</Button>
+                  <Input 
+                  readOnly
+                  fontFamily="inherit"
+                  width="100px"
+                  height="40px"
+                  textAlign="center"
+                  paddingLeft="19px"
+                  marginTop="10px"
+                  type="number" value={mintAmount} />
+                  <Button
+                  backgroundColor="#D6517D"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0F0F0F"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  margin="0 15px" 
+                  onClick={handleIncrement}>+</Button>
+               </Flex>
+               <Button 
+                backgroundColor="#D6517D"
+                  borderRadius="5px"
+                  boxShadow="0px 2px 2px 1px #0F0F0F"
+                  color="white"
+                  cursor="pointer"
+                  fontFamily="inherit"
+                  padding="15px"
+                  marginTop="10px" 
+                  onClick={handleMint}>Mint Now</Button>
             </div>
          ) : (
-            <p>You must be connected to mint!</p>
+            <Text
+            marginTop="70px"
+            fontSize="30px"
+            letterSpacing="-5.5%"
+            fontFamily="VT323"
+            textShadow="0 3px #000000"
+            color="#D6517D"
+            >You must be connected to mint!</Text>
          )}  
-      </div>
+         </Box>
+      </Flex>
    )
 }
 
