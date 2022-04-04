741b5b8db20c0ec032551d2c0229b77c10dff42c
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-05 01:18:51 +0800

    Included NavbBar :gloves:

---
+++ src/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const NavBar = ({ accounts, setAccounts }) => {
+   const isConnected = Boolean(accounts[0]);
+
+   async function connectAccount() {
+      if(window.ethereum) {
+         const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+         });
+         setAccounts(accounts);
+      }
+   }
+
+   return (
+      <div>
+      {/* Left side - social media icons */}
+      <div>Facebook</div>
+      <div>Twitter</div>
+      <div>Instagram</div>
+
+      {/* Right side - Sections and connect to metamask button */}
+      <div>About</div>
+      <div>Mint</div>
+      <div>Team</div>
+
+      {/* Connect */}
+      {/* Check if is connected */}
+      {isConnected ? (
+         <p>Connected</p>
+      ) : (
+         // run the connectAccount function
+         <button onClick={connectAccount}>Connect</button>
+      )}
+      </div>
+   );
+};
