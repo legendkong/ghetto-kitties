cf0f66cef05b110f5734f7c048a768b5e977dc6e
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-05 01:18:12 +0800

    Updated structure of front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+
   return (
     <div className="App">
-   
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
+      <MainMint accounts={accounts} setAccounts={setAccounts} />
     </div>
   );
 }
