f71f3fbb971c3eb88e8e4bd1f864f419c3da8dec
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-05 16:56:09 +0800

    Updated App.js structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import MainMint from './MainMint';
 import NavBar from './NavBar';
+import ImageSwapper from './NFTimages';
 
 function App() {
   const [accounts, setAccounts] = useState([]);
@@ -10,6 +11,7 @@ function App() {
     <div className="overlay">
       <div className="App">
         <NavBar accounts={accounts} setAccounts={setAccounts} />
+        
         <MainMint accounts={accounts} setAccounts={setAccounts} />
       </div>
       <div className="moving-background"></div>
