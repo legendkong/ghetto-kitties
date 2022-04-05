fd159c00b43b2d24e62d3f37857f261211e14c92
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-05 17:06:06 +0800

    Completed rotating images :carousel_horse:

--- src/NFTimages.js
+++ src/NFTimages.js
@@ -10,23 +10,20 @@ import cat5 from './assets/NFTs/cat5.gif';
 const images = [cat1, cat2, cat3, cat4, cat5];
 
 export default function ImageSwapper() {
-   const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentImage, setCurrentImage] = useState(null);
 
-   useEffect(() => {
-      const intervalId = setInterval(() => {
-            if(currentIndex === images.length - 1) {
-               setCurrentIndex(0);
-            } 
-            else {
-               setCurrentIndex(currentIndex + 1);
-            }
-      }, 500) 
-      return () => clearInterval(intervalId);
-   }, [])
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImage(images[Math.floor(Math.random() * images.length)]);
+        }, 500)
+        
+        return () => clearInterval(intervalId);
+    }, [])
+
 
    return (
       <div>
-      <Image src={images[currentIndex]} boxSize="200px" margin="0 5px" />
+      <Image src={currentImage} boxSize="200px" margin="0 5px" />
       </div>
    )  
 }
